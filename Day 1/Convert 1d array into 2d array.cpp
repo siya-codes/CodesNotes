5cb8ef48f72aef7b5de22d6fc258299fa76d063f
Shreya Sharma <106913186+siya-codes@users.noreply.github.com>
2024-09-05 23:55:12 +0530

    Update Convert 1d array into 2d array.cpp

--- Day 1/Convert 1d array into 2d array.cpp
+++ Day 1/Convert 1d array into 2d array.cpp
@@ -29,3 +29,32 @@ public:
 //T.C : O(m*n)
 //S.C : O(1) //no including space of result
 // FOR Both
+
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int m =rolls.size();
+        int sum=mean*(n+m);
+        for(int i=0; i<m; i++){
+        sum-=rolls[i];
+        }
+        if(sum<n ||sum>6*n){
+            return {};
+        }
+        int avg=sum/n;
+        int rem=sum%n;
+        vector<int>res(n,avg);
+        for(int i=0; i<n; i++){
+            res[i]=avg;
+            if(rem>0){
+                res[i]++;
+                rem--;
+            }
+        }
+        return res;
+        
+
+    }
+}; 
+//TC= O(M+N)
+// SC= CONSTANT SPACE
